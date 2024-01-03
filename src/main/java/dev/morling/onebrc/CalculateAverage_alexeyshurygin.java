a8b1e04d34532a36f60ec805c7df331302fc5451
Alexey Shurygin <git@alexeyshurygin.com>
2024-01-04 02:40:32 +0700

    WIP

--- src/main/java/dev/morling/onebrc/CalculateAverage_alexeyshurygin.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_alexeyshurygin.java
@@ -15,49 +15,101 @@
  */
 package dev.morling.onebrc;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Math.min;
+import static java.nio.file.Files.lines;
 
 public class CalculateAverage_alexeyshurygin {
 
     private static final String FILE = "./measurements_short.txt";
 
-    private static void readFile(String filename) throws IOException {
-        try (FileChannel channel = FileChannel.open(Path.of(filename))) {
-            final long size = channel.size();
-            char[] chars = new char[1024 * 1024];
+    private static void readCharFile(String filename) throws IOException {
+        try (var channel = FileChannel.open(Path.of(filename))) {
+            final var size = channel.size();
+            var chars = new char[1024 * 1024];
             long used = 0;
             while (used != size) {
-                long thisSize = min(size - used, MAX_VALUE);
-                CharBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, thisSize).asCharBuffer();
-                // buffer.slice()
-                // CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
-                // CharBuffer charBuffer = decoder.decode(buffer);
-                for (int i = 0; i < buffer.remaining(); i++) {
-                    buffer.get(chars, 0, min(buffer.remaining(), chars.length));
+                var thisSize = min(size - used, MAX_VALUE);
+                var buf = channel.map(FileChannel.MapMode.READ_ONLY, used, thisSize).asCharBuffer();
+                for (int i = 0; i < buf.remaining(); ) {
+                    if (buf.remaining() >= chars.length) {
+                        buf.get(chars, 0, chars.length);
+                        i += chars.length;
+                    } else {
+                        var chars1 = new char[buf.remaining()];
+                        buf.get(chars1, 0, chars1.length);
+                        i += chars1.length;
+                    }
                 }
                 used += thisSize;
             }
         }
     }
 
+    private static void readBBFile(String filename) throws IOException {
+        try (var channel = FileChannel.open(Path.of(filename))) {
+            final var size = channel.size();
+            var bytes = new byte[1024 * 1024];
+            long used = 0;
+            while (used != size) {
+                var thisSize = min(size - used, MAX_VALUE);
+                var buf = channel.map(FileChannel.MapMode.READ_ONLY, used, thisSize);
+                for (int i = 0; i < buf.remaining(); ) {
+                    if (buf.remaining() >= bytes.length) {
+                        buf.get(bytes, 0, bytes.length);
+                        i += bytes.length;
+                    } else {
+                        var bytes1 = new byte[buf.remaining()];
+                        buf.get(bytes1, 0, bytes1.length);
+                        i += bytes1.length;
+                    }
+                }
+                used += thisSize;
+            }
+        }
+    }
+
+    private static void readIOFile(String filename) throws IOException {
+        try (var io = new FileInputStream(filename)) {
+            var bytes = new byte[1024 * 1024];
+            int total = 0;
+            for (int i = 0; i >= 0; i = io.read(bytes)) {
+                total += i;
+            }
+        }
+    }
+
+    private static void readFRFile(String filename) throws IOException {
+        try (var io = new FileReader(filename, StandardCharsets.UTF_8)) {
+            var chars = new char[1024 * 1024];
+            int total = 0;
+            for (int i = 0; i >= 0; i = io.read(chars)) {
+                total += i;
+            }
+        }
+    }
+
     private static void readFileSimple(String filename) throws IOException {
-        Files.lines(Paths.get(filename))
+        lines(Paths.get(filename))
             .forEach(s -> {
             });
     }
 
     public static void main(String[] args) throws IOException {
         long s = System.currentTimeMillis();
-//        readFileSimple(FILE);
-        readFile(FILE);
+//         readFileSimple(FILE);
+        // readCharFile(FILE);
+        // readBBFile(FILE);
+        readIOFile(FILE);
+//        readFRFile(FILE);
         long e = System.currentTimeMillis();
         System.out.println("Time: " + (e - s) + " ms");
     }
